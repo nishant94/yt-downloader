90721657473d262783245b965577f10a5cf2eeae
Nishant Saharan <nishantsaharan94167@gmail.com>
2025-05-22 23:42:52 +0530

    fix build

--- server/logger.ts
+++ server/logger.ts
@@ -1,9 +1,5 @@
 import path from "path";
-import { fileURLToPath } from "url";
 import winston from "winston";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const levels = {
   error: 0,
