324899ff83a48e40804cfd92050a1e3774e2c24a
Nishant Saharan <nishantsaharan94167@gmail.com>
2025-06-18 17:07:09 +0530

    stringify cookie

--- server/services/youtube.ts
+++ server/services/youtube.ts
@@ -28,7 +28,7 @@ export class YoutubeService {
         headers: {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
-          Cookie: cookies,
+          Cookie: JSON.stringify(cookies),
         },
       },
     });
