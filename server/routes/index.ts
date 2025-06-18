dad0b0aff4810f55a7509a7f833665ff784c03f0
Nishant Saharan <nishantsaharan94167@gmail.com>
2025-06-18 16:38:22 +0530

    debug

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -35,7 +35,14 @@ async function requireSession(
 ): Promise<void> {
   const sessionId = req.cookies?.sessionId;
   if (!sessionId) {
-    res.status(401).json({ message: "SessionId not found", sessionId });
+    res
+      .status(401)
+      .json({
+        message: "SessionId not found 4",
+        sessionId,
+        process: process.env.NODE_ENV || "null",
+        domain: process.env.COOKIE_DOMAIN || "null",
+      });
     return;
   }
   if (!(await isValidSession(sessionId))) {
